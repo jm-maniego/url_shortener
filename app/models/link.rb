a25ac01873c13029dad96a4eda87fd12e907a098
jmmaniego <jmmaniego@hotmail.com>
2016-02-01 18:53:18 +0800

    add fake id

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,11 @@
 class Link < ActiveRecord::Base
   after_create :generate_slug
+  def fake_id
+    id && (id + 1_000_000)
+  end
+
   def generate_slug
-    self.slug = id.to_s(36)
+    self.slug = fake_id.to_s(36)
     save!
   end
 end
