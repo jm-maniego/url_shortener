ab508d709f5e01f98172ad0338206e9412967b0f
jmmaniego <jmmaniego@hotmail.com>
2016-02-01 21:57:47 +0800

    refactor links controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,28 +1,25 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: [:show]
+  before_action :set_link, only: [:show, :index, :newly_created]
+  before_action :set_page, only: [:index, :newly_created, :create]
+  before_action :set_new_links, only: [:newly_created, :create]
 
   def index
-    @link  = Link.new
-    page   = params[:page] || 1
-    @top_links = Link.order('clicks DESC').paginate(page: page, per_page: 10)
+    @top_links = Link.order('clicks DESC').paginate(page: @page, per_page: 10)
   end
 
   def newly_created
-    @link  = Link.new
-    page   = params[:page] || 1
-    @new_links = Link.order('created_at DESC').paginate(page: page, per_page: 10)
   end
 
   # GET /links/1
   # GET /links/1.json
   def show
-    if params[:slug]
+    if params[:slug] && params[:controller] != params[:slug]
       if @link.present? && redirect_to(@link.given_url)
         @link.clicks += 1
         @link.save!
       end
     elsif params[:id]
-
+      @link = Link.find(params[:id])
     end
   end
 
@@ -36,7 +33,7 @@ class LinksController < ApplicationController
         format.html { redirect_to newly_created_links_path, notice: 'Link was successfully created.' }
         format.json { render :show, status: :created, location: @link }
       else
-        format.html { render :new }
+        format.html { render :newly_created }
         format.json { render json: @link.errors, status: :unprocessable_entity }
       end
     end
@@ -45,7 +42,16 @@ class LinksController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_link
-      @link = Link.find_by(slug: params[:slug])
+      # wanted to use find_or_initialize by but it will instantiate with values
+      @link = Link.find_by(slug: params[:slug]) || Link.new
+    end
+
+    def set_page
+      @page = params[:page] || 1
+    end
+
+    def set_new_links
+      @new_links = Link.order('created_at DESC').paginate(page: @page, per_page: 10)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
